f95fb3eb85c3008ce0c82d901ae6c7fceb2a9600
jlluch <jlluch@ai2.upv.es>
2022-12-03 18:23:15 +0100

    Update MapaPrecioCarburantes.py

--- MapaPrecioCarburantes.py
+++ MapaPrecioCarburantes.py
@@ -41,7 +41,7 @@ def rgb_to_hex(rgb):
 @st.cache
 def cargarFichero():
     URL = "https://geoportalgasolineras.es/resources/files/preciosEESS_es.xls"
-    df = pd.read_excel(URL, skiprows=3, engine="openpyxl")
+    df = pd.read_excel(URL, skiprows=3)
     # Provincia	Municipio	Localidad	Código postal	Dirección	Margen	Longitud	Latitud	Toma de datos	
     # Precio gasolina 95 E5	Precio gasolina 95 E10	Precio gasolina 95 E5 Premium	Precio gasolina 98 E5	Precio gasolina 98 E10	Precio gasóleo A	Precio gasóleo Premium	Precio gasóleo B	Precio gasóleo C	Precio bioetanol	% bioalcohol	Precio biodiésel	% éster metílico	Precio gases licuados del petróleo	Precio gas natural comprimido	Precio gas natural licuado	Precio hidrógeno	Rótulo	Tipo venta	Rem.	Horario	Tipo servicio       
     elim = ['MELILLA','CEUTA','PALMAS (LAS)','SANTA CRUZ DE TENERIFE']

86076bf1cfe9e78ce5c54785e65819396302f6c2
jlluch <jlluch@ai2.upv.es>
2022-12-04 16:32:38 +0100

    Update MapaPrecioCarburantes.py

--- MapaPrecioCarburantes.py
+++ MapaPrecioCarburantes.py
@@ -41,7 +41,7 @@ def rgb_to_hex(rgb):
 @st.cache
 def cargarFichero():
     URL = "https://geoportalgasolineras.es/resources/files/preciosEESS_es.xls"
-    df = pd.read_excel(URL, skiprows=3)
+    df = pd.read_excel(URL, skiprows=3, engine="xlrd")
     # Provincia	Municipio	Localidad	Código postal	Dirección	Margen	Longitud	Latitud	Toma de datos	
     # Precio gasolina 95 E5	Precio gasolina 95 E10	Precio gasolina 95 E5 Premium	Precio gasolina 98 E5	Precio gasolina 98 E10	Precio gasóleo A	Precio gasóleo Premium	Precio gasóleo B	Precio gasóleo C	Precio bioetanol	% bioalcohol	Precio biodiésel	% éster metílico	Precio gases licuados del petróleo	Precio gas natural comprimido	Precio gas natural licuado	Precio hidrógeno	Rótulo	Tipo venta	Rem.	Horario	Tipo servicio       
     elim = ['MELILLA','CEUTA','PALMAS (LAS)','SANTA CRUZ DE TENERIFE']
@@ -97,7 +97,13 @@ def create_map(dfDib, prov_data):
     
     if (location != None):
         folium.Marker([location.get('coords').get('latitude'), location.get('coords').get('longitude')],radius=500,popup="Mi posición",color="#3186cc",fill=True,fill_color="#3186cc").add_to(m)
-    folium_static(m, width=900, height=600)
+    w = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
+    h = streamlit_js_eval(js_expressions='screen.heigth', key = 'SCR')
+
+    if w>h :
+        folium_static(m, width=900, height=600)
+    else:
+        folium_static(m, width=600, height=900)
    
 
 def display_precios_provincia(df, prov_name):
@@ -146,12 +152,3 @@ for i in range(x+1):
     st.write(baratas.Localidad.iat[i]+' : '+baratas['Dirección'].iat[i]+' -- '+str(baratas['Código postal'].iat[i])+' -- Horario: '+str(baratas['Horario'].iat[i])+' --  Precio: '+str(baratas[combustible].iat[i])+' €')
 
 display_precios_provincia(prov_data, provincia)
-
-
-
-
-
-   
-
-
-

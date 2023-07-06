7e9d0fdd983161803e042f5b6cf5e89353d06867
Zachary Blackwood <zachary.blackwood@snowflake.com>
2023-07-06 10:13:34 -0400

    Update MapaPrecioCarburantes.py

--- MapaPrecioCarburantes.py
+++ MapaPrecioCarburantes.py
@@ -50,7 +50,7 @@ def cargarFichero():
 
     res = requests.get(URL)
     st.write(res)
-    df = pd.read_excel((io.BytesIO(res.content), skiprows=3, engine="xlrd")
+    df = pd.read_excel(io.BytesIO(res.content), skiprows=3, engine="xlrd")
     # Provincia	Municipio	Localidad	Código postal	Dirección	Margen	Longitud	Latitud	Toma de datos	
     # Precio gasolina 95 E5	Precio gasolina 95 E10	Precio gasolina 95 E5 Premium	Precio gasolina 98 E5	Precio gasolina 98 E10	Precio gasóleo A	Precio gasóleo Premium	Precio gasóleo B	Precio gasóleo C	Precio bioetanol	% bioalcohol	Precio biodiésel	% éster metílico	Precio gases licuados del petróleo	Precio gas natural comprimido	Precio gas natural licuado	Precio hidrógeno	Rótulo	Tipo venta	Rem.	Horario	Tipo servicio       
     elim = ['MELILLA','CEUTA','PALMAS (LAS)','SANTA CRUZ DE TENERIFE']

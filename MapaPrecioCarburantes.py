70d4afb00c940da7ec946859be40365219f1960d
Zachary Blackwood <zachary.blackwood@snowflake.com>
2023-07-06 10:13:15 -0400

    Update MapaPrecioCarburantes.py

--- MapaPrecioCarburantes.py
+++ MapaPrecioCarburantes.py
@@ -42,9 +42,15 @@ def rgb_to_hex(rgb):
 @st.cache_data(ttl=86400) 
 def cargarFichero():
     FAct = "Actualizado: "+datetime.now(tz=pytz.timezone('Europe/Madrid')).strftime("%d/%m/%Y %H:%M")
+    import requests
+    import urllib3
+    import io
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
     URL = "http://geoportalgasolineras.es/resources/files/preciosEESS_es.xls"
-    st.write(URL)
-    df = pd.read_excel(URL, skiprows=3, engine="xlrd")
+
+    res = requests.get(URL)
+    st.write(res)
+    df = pd.read_excel((io.BytesIO(res.content), skiprows=3, engine="xlrd")
     # Provincia	Municipio	Localidad	Código postal	Dirección	Margen	Longitud	Latitud	Toma de datos	
     # Precio gasolina 95 E5	Precio gasolina 95 E10	Precio gasolina 95 E5 Premium	Precio gasolina 98 E5	Precio gasolina 98 E10	Precio gasóleo A	Precio gasóleo Premium	Precio gasóleo B	Precio gasóleo C	Precio bioetanol	% bioalcohol	Precio biodiésel	% éster metílico	Precio gases licuados del petróleo	Precio gas natural comprimido	Precio gas natural licuado	Precio hidrógeno	Rótulo	Tipo venta	Rem.	Horario	Tipo servicio       
     elim = ['MELILLA','CEUTA','PALMAS (LAS)','SANTA CRUZ DE TENERIFE']
